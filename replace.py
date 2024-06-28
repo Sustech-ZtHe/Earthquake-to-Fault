15f0468e19e20d979b200b2a0026b9bfde44a978
Zhengtao He <128665415+User0324-student@users.noreply.github.com>
2024-06-28 16:29:02 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,15 +1,18 @@
 import shutil
 
+hough3dlines_road="..."
+E2F_road="..."
+
 # 源文件路径
-origin_h3d_main= "/home/me/Documents/Ridgecrest/hough3dlines.cpp"
-origin_pointcloud= "/home/me/Documents/Ridgecrest/pointcloud.cpp"
-origin_vector3d= "/home/me/Documents/Ridgecrest/vector3d.cpp"
-origin_vector3d_h= "/home/me/Documents/Ridgecrest/vector3d.h"
+origin_h3d_main= f"{hough3dlines_road}/hough3dlines.cpp"
+origin_pointcloud= f"{hough3dlines_road}/pointcloud.cpp"
+origin_vector3d= f"{hough3dlines_road}/vector3d.cpp"
+origin_vector3d_h= f"{hough3dlines_road}/vector3d.h"
 # 替换文件路径
-replace_h3d_main = "/home/me/Documents/replace_code/hough3dlines_replaced.cpp"
-replace_pointcloud= "/home/me/Documents/replace_code/pointcloud_replaced.cpp"
-replace_vector3d= "/home/me/Documents/replace_code/vector3d_replaced.cpp"
-replace_vector3d_h= "/home/me/Documents/replace_code/vector3d_replaced.h"
+replace_h3d_main = f"{E2F_road}/replace_code/hough3dlines_replaced.cpp"
+replace_pointcloud= f"{E2F_road}/replace_code/pointcloud_replaced.cpp"
+replace_vector3d= f"{E2F_road}/replace_code/vector3d_replaced.cpp"
+replace_vector3d_h= f"{E2F_road}/replace_code/vector3d_replaced.h"
 
 # 将 replace 的内容复制到 origin
 shutil.copyfile(replace_h3d_main, origin_h3d_main)
