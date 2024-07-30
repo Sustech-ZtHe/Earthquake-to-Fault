e015ab3d1253fdd392c2c22052d733290911f0e2
Zhengtao He <128665415+User0324-student@users.noreply.github.com>
2024-07-30 18:57:40 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,21 +1,32 @@
 import shutil
-
-hough3dlines_road="..."
-E2F_road="..."
-
+import os
+current_path=os.getcwd()
+dir_path=os.path.dirname(current_path)
+h3d_road=f"{dir_path}/hough-3d-lines-master"
+e2f_road=f"{dir_path}/Earthquake-to-Fault-main"
 # 源文件路径
-origin_h3d_main= f"{hough3dlines_road}/hough3dlines.cpp"
-origin_pointcloud= f"{hough3dlines_road}/pointcloud.cpp"
-origin_vector3d= f"{hough3dlines_road}/vector3d.cpp"
-origin_vector3d_h= f"{hough3dlines_road}/vector3d.h"
+#origin_h3d_main= "/your_hough-3d-line_road/hough3dlines.cpp"
+#origin_pointcloud= "/your_hough-3d-line_road/pointcloud.cpp"
+#origin_vector3d= "/your_hough-3d-line_road/vector3d.cpp"
+#origin_vector3d_h= "/your_hough-3d-line_road/vector3d.h"
+origin_h3d_main= f"{h3d_road}/hough3dlines.cpp"
+origin_pointcloud= f"{h3d_road}/pointcloud.cpp"
+origin_vector3d= f"{h3d_road}/vector3d.cpp"
+origin_vector3d_h= f"{h3d_road}/vector3d.h"
 # 替换文件路径
-replace_h3d_main = f"{E2F_road}/replace_code/hough3dlines_replaced.cpp"
-replace_pointcloud= f"{E2F_road}/replace_code/pointcloud_replaced.cpp"
-replace_vector3d= f"{E2F_road}/replace_code/vector3d_replaced.cpp"
-replace_vector3d_h= f"{E2F_road}/replace_code/vector3d_replaced.h"
+replace_h3d_main = f"{e2f_road}/replace_code/hough3dlines_replaced.cpp"
+replace_pointcloud= f"{e2f_road}/replace_code/pointcloud_replaced.cpp"
+replace_vector3d= f"{e2f_road}/replace_code/vector3d_replaced.cpp"
+replace_vector3d_h= f"{e2f_road}/replace_code/vector3d_replaced.h"
+
+e2f_package=f"{e2f_road}/E2F_package"
+savedata_package=f"{e2f_road}/SaveData"
 
 # 将 replace 的内容复制到 origin
 shutil.copyfile(replace_h3d_main, origin_h3d_main)
 shutil.copyfile(replace_pointcloud, origin_pointcloud)
 shutil.copyfile(replace_vector3d, origin_vector3d)
 shutil.copyfile(replace_vector3d_h, origin_vector3d_h)
+
+shutil.copytree(e2f_package, os.path.join(h3d_road,os.path.basename(e2f_package)))
+shutil.copytree(savedata_package, os.path.join(h3d_road,os.path.basename(e2f_package)))
