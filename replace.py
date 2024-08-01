f75fd50bab2284be5f7a4a4dff67bb1747ec6b0c
Zhengtao He <128665415+User0324-student@users.noreply.github.com>
2024-08-01 11:19:53 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -4,6 +4,7 @@ current_path=os.getcwd()
 dir_path=os.path.dirname(current_path)
 h3d_road=f"{dir_path}/hough-3d-lines-master"
 e2f_road=f"{dir_path}/Earthquake-to-Fault-main"
+e2fpackage_road=f"{dir_path}/Earthquake-to-Fault-main/E2F_package"
 # 源文件路径
 #origin_h3d_main= "/your_hough-3d-line_road/hough3dlines.cpp"
 #origin_pointcloud= "/your_hough-3d-line_road/pointcloud.cpp"
@@ -19,8 +20,6 @@ replace_pointcloud= f"{e2f_road}/replace_code/pointcloud_replaced.cpp"
 replace_vector3d= f"{e2f_road}/replace_code/vector3d_replaced.cpp"
 replace_vector3d_h= f"{e2f_road}/replace_code/vector3d_replaced.h"
 
-e2f_package=f"{e2f_road}/E2F_package"
-savedata_package=f"{e2f_road}/SaveData"
 
 # 将 replace 的内容复制到 origin
 shutil.copyfile(replace_h3d_main, origin_h3d_main)
@@ -28,5 +27,4 @@ shutil.copyfile(replace_pointcloud, origin_pointcloud)
 shutil.copyfile(replace_vector3d, origin_vector3d)
 shutil.copyfile(replace_vector3d_h, origin_vector3d_h)
 
-shutil.copytree(e2f_package, os.path.join(h3d_road,os.path.basename(e2f_package)))
-shutil.copytree(savedata_package, os.path.join(h3d_road,os.path.basename(e2f_package)))
+shutil.move(e2fpackage_road,h3d_road)
