93212b2f0176c19a00fd5a868eff280e18c974bc
Zhengtao He <128665415+Sustech-ZtHe@users.noreply.github.com>
2025-06-20 15:11:05 +0800

    Update Scatter3_E2F.m

--- E2F_package/Scatter3_E2F.m
+++ E2F_package/Scatter3_E2F.m
@@ -5,15 +5,12 @@ if ~isempty(points)
         if ismember(points(:,1)<0,1)
             scatter3(abs(points(:, 1)), points(:, 2), points(:, 3), psize, points(:,11:13),"filled");view(0,90);
             ax=gca;set(ax,'Zdir','reverse','Xdir','reverse','YMinorTick','on','XMinorTick','on','ZMinorTick','on','FontSize',12,'color',[0.9 0.9 0.9]);
-            ax.XTickLabel=strcat(ax.XTickLabel, '°W');
             box on;grid off;grid minor
         else
             scatter3(points(:, 1), points(:, 2), points(:, 3),psize, points(:,11:13),"filled");view(0,90);
             ax=gca;set(ax,'Zdir','reverse','YMinorTick','on','XMinorTick','on','ZMinorTick','on','FontSize',12,'color',[0.9 0.9 0.9]);
-            ax.XTickLabel=strcat(ax.XTickLabel, '°E');
             box on;grid off;grid minor
         end
-        ax.YTickLabel=strcat(ax.YTickLabel, '°N');
         pos = get(ax, 'Position');
         annotation('textbox', [pos(1)+pos(3)*0.4, pos(2)+pos(4)*0.05, 0.8*pos(3), 0.1*pos(4)], 'LineStyle','none',...
                     'String', ['Fault candidates=',num2str(length(unique(points(:,14)))),newline,'Clustered events=',num2str(size(points,1)),newline,'Remaining events=',num2str(size(hypo,1)-size(points,1)) ], ...
@@ -26,15 +23,12 @@ if ~isempty(points)
         if min(points(:,1))<0
             scatter3(abs(points(:, 1)), points(:, 2), points(:, 3),psize, points(:,11:13),"filled");view(0,90)
             ax=gca;set(ax,'Zdir','reverse','Xdir','reverse','YMinorTick','on','XMinorTick','on','ZMinorTick','on','FontSize',15,'color',[0.9 0.9 0.9]);
-            ax.XTickLabel=strcat(ax.XTickLabel, '°W');
             box on;grid off;grid off
         else
             scatter3(points(:, 1), points(:, 2), points(:, 3),psize, points(:,11:13),"filled");view(0,90)
             ax=gca;set(ax,'Zdir','reverse','YMinorTick','on','XMinorTick','on','ZMinorTick','on','FontSize',15,'color',[0.9 0.9 0.9]);
-            ax.XTickLabel=strcat(ax.XTickLabel, '°E');
             box on;grid off;grid off
         end
-        ax.YTickLabel=strcat(ax.YTickLabel, '°N');
         pos = get(ax, 'Position');
         if name~=char(10)
         annotation('textbox', [pos(1)+pos(3)*0.01, pos(2)+pos(4)*0.08, 1.5*pos(3), 0.01*pos(4)], 'LineStyle','none',...
@@ -50,38 +44,39 @@ if ~isempty(points)
             if min(points(:,1))<0
                 scatter3(abs(points(:, 1)), points(:, 2), points(:, 3),psize, points(:,4:6),"filled");view(0,90);
                 ax=gca;set(ax,'Zdir','reverse','Xdir','reverse','YMinorTick','on','XMinorTick','on','ZMinorTick','on','FontSize',12,'color',[0.9 0.9 0.9]);
-                ax.XTickLabel=strcat(ax.XTickLabel, '°W');
                 box on;grid off;grid minor
             else
                 scatter3(points(:, 1), points(:, 2), points(:, 3),psize, points(:,4:6),"filled");view(0,90);
                 ax=gca;set(ax,'Zdir','reverse','YMinorTick','on','XMinorTick','on','ZMinorTick','on','FontSize',12,'color',[0.9 0.9 0.9]);
-                ax.XTickLabel=strcat(ax.XTickLabel, '°E');
                 box on;grid off;grid minor
             end
-            ax.YTickLabel=strcat(ax.YTickLabel, '°N');
         else
             if min(points(:,1))<0
                 scatter3(abs(points(:, 1)), points(:, 2), points(:, 3),psize, points(:,11:13),"filled");view(0,90);
                 ax=gca;set(ax,'Zdir','reverse','Xdir','reverse','YMinorTick','on','XMinorTick','on','ZMinorTick','on','FontSize',12,'color',[0.9 0.9 0.9]);
-                ax.XTickLabel=strcat(ax.XTickLabel, '°W');
                 box on;grid off;grid minor
             else
                 scatter3(points(:, 1), points(:, 2), points(:, 3),psize, points(:,11:13),"filled");view(0,90);
                 ax=gca;set(ax,'Zdir','reverse','YMinorTick','on','XMinorTick','on','ZMinorTick','on','FontSize',12,'color',[0.9 0.9 0.9]);
-                ax.XTickLabel=strcat(ax.XTickLabel, '°E');
                 box on;grid off;grid minor
             end
-            ax.YTickLabel=strcat(ax.YTickLabel, '°N');
         end
     end
     d1=(max(hypo(:,9))-min(hypo(:,9)))/50;
     d2=(max(hypo(:,8))-min(hypo(:,8)))/50;
     if min(points(:,1))<0
         xlim([abs(max(hypo(:,9))+d1) abs(min(hypo(:,9))-d1)]);
-        ylim([min(hypo(:,8))-d2 max(hypo(:,8))+d2]); 
+        ax.XTickLabel = string(ax.XTick) + "°W";
     else
         xlim([min(hypo(:,9))-d1 max(hypo(:,9))+d1]);
+        ax.XTickLabel = string(ax.XTick) + "°E";
+    end
+    if min(points(:,2))>0
         ylim([min(hypo(:,8))-d2 max(hypo(:,8))+d2]);
+        ax.YTickLabel = string(ax.YTick) + "°N";
+    else
+        ylim([abs(max(hypo(:,8))+d2) abs(min(hypo(:,8))-d2)]);
+        ax.YTickLabel = string(ax.YTick) + "°S";
     end
 end
 
