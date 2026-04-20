33ec425b9120245c5cdccf13d21df9bd45dfd838
Zhengtao He <128665415+Sustech-ZtHe@users.noreply.github.com>
2026-04-20 20:24:42 +0800

    Update Mmain.m

--- E2F_package/Mmain.m
+++ E2F_package/Mmain.m
@@ -1,11 +1,23 @@
-function [a,b,M]=Mmain(ftest4,hypotout)
-    ev_mag=ftest4(:,4);
-    mc=calc_McMaxCurvature(ev_mag);
-    mag_sel = ev_mag(ev_mag >= mc+0.2);
-    [b, bstd, a] =  calc_bmemag(mag_sel, 0.01);
-    N=1;
-    M=(log10(N)-a)/b;
+function [a,b,M] = Mmain(ftest4, hypotout)
+
+    ev_mag = ftest4(:,4);
+
+    if numel(ev_mag) < 5
+        a = NaN; b = NaN; M = NaN;
+        return
+    end
+
+    mc = calc_McMaxCurvature(ev_mag);
+
+    mag_sel = ev_mag(ev_mag >= mc + 0.2);
+
+    if numel(mag_sel) < 2
+        a = NaN; b = NaN; M = NaN;
+        return
+    end
+
+    [b, bstd, a] = calc_bmemag(mag_sel, 0.01);
+
+    N = 1;
+    M = (log10(N) - a) / b;
 end
-
-
-
